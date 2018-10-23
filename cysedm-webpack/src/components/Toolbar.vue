7731f6a8ab9e0794b74afda9c149fd4754564a56
H. Bonnes <herman.bonnes@hva.nl>
2018-10-23 21:17:07 +0200

    ajb

--- cysedm-webpack/src/components/Toolbar.vue
+++ cysedm-webpack/src/components/Toolbar.vue
@@ -6,6 +6,26 @@
       <v-toolbar-title class="white--text">CyseDM</v-toolbar-title>
     </router-link>
     <v-spacer></v-spacer>
+    
+    <!-- alert -->
+    <v-alert
+      style="position: relative; top: 62px;"
+      v-model="alertSuccess"
+      transition="scale-transition"
+      type="success"
+      icon="check_circle"
+      dismissible
+    >You've succesfully logged out!</v-alert>
+    <v-alert
+      style="position: absolute; top: 62px;"
+      v-model="alertFail"
+      transition="scale-transition"
+      type="error"
+      icon="warning"
+      dismissible
+    >Error logging out: {{err}}!</v-alert>
+
+    <!-- Logout button -->
     <v-menu v-if="isLoggedIn" offset-y>
       <v-btn flat slot="activator">
         <span>{{ currentUser.username }}</span>
@@ -26,12 +46,22 @@ import { mapGetters } from "vuex";
 export default {
   data() {
     return {
+      alertSuccess: false,
+      alertFail: false,
+      err: "",
       actions: [
         {
           title: "Logout",
           action: () => {
-            this.$store.commit("users/logout");
-            this.$router.push('login');
+            try {
+              this.$store.commit("users/logout");
+              this.$router.push("login");
+              this.alertSuccess = true;
+              setTimeout(function(){ this.alertSuccess = false; }, 3000);
+            } catch (err) {
+              this.err = err;
+              this.alertFail = true;
+            }
           }
         }
       ]
@@ -40,9 +70,12 @@ export default {
   computed: {
     ...mapGetters("users", ["isLoggedIn", "currentUser"])
   },
+  watch: {
+
+  },
   methods: {
     toggleDrawer() {
-      this.$eventHub.$emit('toggleDrawer');
+      this.$eventHub.$emit("toggleDrawer");
     }
   }
 };
