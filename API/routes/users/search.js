c0a3cde7ab419d6d5a2fd25dda621767287656c4
ZENBOOK-HAGEN\THage <t.hagen@hotmail.nl>
2018-10-26 17:20:21 +0200

    End-point search user

---
+++ API/routes/users/search.js
@@ -0,0 +1,36 @@
+// Models
+const Db = require('../../models');
+// Error messages
+const Boom = require('boom');
+
+module.exports = [
+    {
+        method: 'GET',
+        path: '/api/v1/user/list/{name?}',
+        config: {
+            auth: {
+                strategy: 'jwt',
+                scope: 'user'
+            }
+        },
+        handler: async (req, h) => {
+            try {
+                const Op = Db.Sequelize.Op;
+                // Search for a user by username or email
+                let result = await Db.User.findOne({
+                    attributes: {exclude: ['admin', 'password', 'created']},
+                    where: {
+                        [Op.and]: [
+                        { [Op.or]: [{ "username": req.params.name }, { "email": req.params.name }] },
+                        { [Op.not]: { id: req.auth.credentials.id }}
+                        ]
+                    },                   
+                    order: Db.Sequelize.col('username')
+                })
+                return h.response(result).code(200);         
+            } catch (err) {
+                return Boom.badImplementation(`Could not load users. Error: ${err}`)
+            }
+        }
+    }
+]
