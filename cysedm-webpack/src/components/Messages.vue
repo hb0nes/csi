e97e0c60835f585479627c06badc3fcd253accfe
Herman Bonnes <h.bonnes@gmail.com>
2018-10-26 17:33:25 +0200

    Added search user functionality

--- cysedm-webpack/src/components/Messages.vue
+++ cysedm-webpack/src/components/Messages.vue
@@ -2,10 +2,48 @@
   <v-app>
     <v-navigation-drawer width="500" app clipped v-model="drawer">
       <v-layout>
+        <v-dialog v-model="dialog" max-width="500px">
+          <v-card>
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>Select Recipient</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-text-field
+                label="Search"
+                append-icon="search"
+                @click:append="searchUser(search)"
+                @keyup.enter="searchUser(search)"
+                v-model="search"
+                solo
+              ></v-text-field>
+              <p v-if="searchMsg.length > 0">{{ searchMsg }}</p>
+              <v-list v-if="searchResult.length > 0" two-line>
+                <template v-for="(item, index) in searchResult">
+                  <v-list-tile :key="index" avatar @click="addUser">
+                    <v-list-tile-avatar>
+                      <img :src="item.avatar">
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                      <v-list-tile-title>{{ item.firstName }} {{ item.lastName }}</v-list-tile-title>
+                      <v-list-tile-sub-title>{{ item.status }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </template>
+              </v-list>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" flat @click="dialog=false;searchResult=[]; search=''">OK</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
         <v-flex>
           <v-toolbar color="blue lighten-5">
             <v-toolbar-title>Conversations</v-toolbar-title>
             <v-spacer></v-spacer>
+            <v-btn icon @click="dialog = true">
+              <v-icon>create</v-icon>
+            </v-btn>
             <v-btn icon>
               <v-icon>search</v-icon>
             </v-btn>
@@ -40,18 +78,33 @@
                   {{getDate(msg.datetime)}}
                   <v-divider></v-divider>
                 </div>
-                </template>
-                <span :class="messageStyle(msg.sender)">
-                  {{msg.content}}<br>                  
-                  <em style="font-size: 0.7em">{{getTime(msg.datetime)}}</em>
-                </span>             
+              </template>
+              <span :class="messageStyle(msg.sender)">
+                {{msg.content}}
+                <br>
+                <em style="font-size: 0.7em">{{getTime(msg.datetime)}}</em>
+              </span>
             </v-flex>
           </v-layout>
         </v-container>
       </div>
       <div class="typer">
-        <input v-if="currentPartner" id="msgBox" v-model="msgContent"  @keyup.enter="sendMsg" placeholder="Type a message" type="text">
-        <v-btn v-if="currentPartner" :disabled="msgContent.length < 1" flat color="primary" class="subheading" @click="sendMsg">Send</v-btn>
+        <input
+          v-if="currentPartner"
+          id="msgBox"
+          v-model="msgContent"
+          @keyup.enter="sendMsg"
+          placeholder="Type a message"
+          type="text"
+        >
+        <v-btn
+          v-if="currentPartner"
+          :disabled="msgContent.length < 1"
+          flat
+          color="primary"
+          class="subheading"
+          @click="sendMsg"
+        >Send</v-btn>
       </div>
     </v-content>
   </v-app>
@@ -68,8 +121,12 @@ export default {
       messages: [],
       currentPartner: "",
       debug: false,
+      dialog: false,
       drawer: true,
       msgContent: "",
+      search: "",
+      searchResult: {},
+      searchMsg: ""
     };
   },
   sockets: {
@@ -97,6 +154,30 @@ export default {
     }
   },
   methods: {
+    addUser() {
+      return;
+    },
+    // Find a user
+    searchUser(username) {
+      // Bij een axios catch
+      if (username.toLowerCase() !== "robert") {
+        this.searchResult = [];
+        this.searchMsg = "No users found.";
+      } else {
+        this.searchMsg = "";
+        this.searchResult = [
+          {
+            username: "Robert",
+            firstName: "Robert",
+            lastName: "Knook",
+            avatar:
+              "https://openclipart.org/image/2400px/svg_to_png/202776/pawn.png",
+            email: "robertjanknook@gmail.nl",
+            status: "Ik ben awesome."
+          }
+        ];
+      }
+    },
     // Scroll to last message
     scrollBottom() {
       this.$nextTick(function() {
@@ -104,8 +185,8 @@ export default {
         container.scrollTop = container.scrollHeight;
       });
     },
-    alert(){
-      alert("BLA")
+    alert() {
+      alert("BLA");
     },
     toggleDrawer() {
       this.drawer = !this.drawer;
@@ -114,14 +195,16 @@ export default {
     sendMsg() {
       if (this.msgContent.length > 0 && this.currentUser) {
         // Add your sent message to the page without doing another API call
-        
-        let dt = new Date()
+
+        let dt = new Date();
         let offset = dt.getTimezoneOffset();
         this.messages.push({
           sender: this.currentUser,
           receiver: this.currentPartner,
           content: this.msgContent,
-          datetime: moment(dt).add(offset, "m").format("YYYY-MM-DD HH:mm:ss")
+          datetime: moment(dt)
+            .add(offset, "m")
+            .format("YYYY-MM-DD HH:mm:ss")
         });
         // Clear input field
         let msg = this.msgContent;
@@ -135,7 +218,9 @@ export default {
             receiver: this.currentPartner
           },
           withCredentials: true,
-          url: `http://${process.env.VUE_APP_SERVERNAME}:3000/api/v1/message/create`
+          url: `http://${
+            process.env.VUE_APP_SERVERNAME
+          }:3000/api/v1/message/create`
         })
           .then(() => {})
           .catch(err => {
@@ -150,7 +235,9 @@ export default {
       this.axios({
         method: "GET",
         withCredentials: true,
-        url: `http://${process.env.VUE_APP_SERVERNAME}:3000/api/v1/message/partners`
+        url: `http://${
+          process.env.VUE_APP_SERVERNAME
+        }:3000/api/v1/message/partners`
       })
         .then(res => {
           this.partners = res.data;
@@ -167,7 +254,9 @@ export default {
       this.axios({
         method: "GET",
         withCredentials: true,
-        url: `http://${process.env.VUE_APP_SERVERNAME}:3000/api/v1/message/read/${partner}`
+        url: `http://${
+          process.env.VUE_APP_SERVERNAME
+        }:3000/api/v1/message/read/${partner}`
       })
         .then(res => {
           this.messages = res.data;
@@ -193,22 +282,26 @@ export default {
       return classes;
     },
     newDate(nextDate, currDate) {
-      return (this.getDate(nextDate) != this.getDate(currDate))       
+      return this.getDate(nextDate) != this.getDate(currDate);
     },
-    getDate(datetime){
-      moment.locale('nl');
+    getDate(datetime) {
+      moment.locale("nl");
       var date = new Date();
       let offset = date.getTimezoneOffset();
       let dt = moment(datetime, "YYYY-MM-DD HH:mm:ss");
-      return moment(dt).add(-offset, "m").format("l");
+      return moment(dt)
+        .add(-offset, "m")
+        .format("l");
     },
-    getTime(datetime){
-      moment.locale('nl');
-      var date = new Date();     
+    getTime(datetime) {
+      moment.locale("nl");
+      var date = new Date();
       let offset = date.getTimezoneOffset();
       let dt = moment(datetime, "YYYY-MM-DD HH:mm:ss");
-      return moment(dt).add(-offset, "m").format("HH:mm");
-    },
+      return moment(dt)
+        .add(-offset, "m")
+        .format("HH:mm");
+    }
   },
   computed: {
     currentUser: function() {
