2f2672583d3b980736af21ddd4ed04a0f731a864
Robert <robert@localhost.localdomain>
2019-01-10 14:16:43 +0100

    first test with apm node

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,15 @@
+// Add this to the VERY top of the first file loaded in your app
+const apm = require('elastic-apm-node').start({
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    serviceName: 'cysedm',
+  
+    // Use if APM Server requires a token
+    // secretToken: '',
+  
+    // Set custom APM Server URL (default: http://localhost:8200)
+    //serverUrl: '',
+  })
 'use strict';
 const fs = require('fs');
 
